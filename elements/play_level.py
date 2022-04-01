6f4dc76379843931f3b980f17a39e53211e90b14
Gospodin <danya.zvyagin@gmail.com>
2022-04-01 10:47:47 +0300

    palletes fix

--- elements/play_level.py
+++ elements/play_level.py
@@ -44,9 +44,11 @@ class PlayLevel(GameStrategy):
         self.objects_for_tp = []
 
         self.win_offsets = [[(775, 325), 0], [(825, 325), 0], [(725, 325), 0], [(875, 325), 0], [(675, 325), 0],
-                            [(925, 325), 0], [(625, 325), 0], [(975, 325), 0], [(575, 325), 0], [(1025, 325), 0],
-                            [(525, 325), 0], [(1075, 325), 0], [(475, 325), 0], [(1100, 325), 0], [(450, 325), 0],
-                            [(1125, 325), 0], [(425, 325), 0]]
+                            [(925, 325), 0], [(625, 325), 0], [
+            (975, 325), 0], [(575, 325), 0], [(1025, 325), 0],
+            [(525, 325), 0], [(1075, 325), 0], [
+            (475, 325), 0], [(1100, 325), 0], [(450, 325), 0],
+            [(1125, 325), 0], [(425, 325), 0]]
         self.flag_to_win_animation = False
         self.flag_to_delay = False
         self.win_text = self.text_to_png('congratulations')
@@ -262,23 +264,7 @@ class PlayLevel(GameStrategy):
                    (300, 900), (800, 900), (1200, 900), (0, 300),
                    (0, 600), (1600, 100), (1600, 500), (1600, 900)]
         for offset in offsets:
-            pygame.draw.circle(self.screen, (0, 50, 30),
-                               offset, self.circle_radius)
-        if pygame.time.get_ticks() - self.delay <= 3000:
-            for character_object in self.level_name_object_text:
-                character_object.draw(self.screen)
-        if pygame.time.get_ticks() - self.delay > 3000:
-            self.circle_radius -= 8
-        if self.circle_radius <= 0:
-            self.circle_radius = 0
-            self.flag_to_level_start_animation = False
-
-    def level_start_animation(self):
-        offsets = [(0, 0), (600, 0), (1000, 0), (1600, 0), (0, 900),
-                   (300, 900), (800, 900), (1200, 900), (0, 300),
-                   (0, 600), (1600, 100), (1600, 500), (1600, 900)]
-        for offset in offsets:
-            pygame.draw.circle(self.screen, (0, 50, 30),
+            pygame.draw.circle(self.screen, self.current_palette.pixels[3][6],
                                offset, self.circle_radius)
         if pygame.time.get_ticks() - self.delay <= 3000:
             for character_object in self.level_name_object_text:
@@ -296,13 +282,15 @@ class PlayLevel(GameStrategy):
         max_radius = 100
         if not self.flag_to_level_start_animation and self.flag_to_win_animation:
             for offset, radius in self.win_offsets:
-                pygame.draw.circle(self.screen, (0, 50, 30),
+                pygame.draw.circle(self.screen, self.current_palette.pixels[3][6],
                                    offset, radius)
             if self.win_offsets[0][1] < max_radius:
                 self.win_offsets[0][1] += 0.1 * (len(self.win_offsets))
                 for index in range(1, len(self.win_offsets), 2):
-                    self.win_offsets[index][1] += 0.1 * (len(self.win_offsets) - index)
-                    self.win_offsets[index + 1][1] += 0.1 * (len(self.win_offsets) - index)
+                    self.win_offsets[index][1] += 0.1 * \
+                        (len(self.win_offsets) - index)
+                    self.win_offsets[index + 1][1] += 0.1 * \
+                        (len(self.win_offsets) - index)
 
             if self.win_offsets[0][1] >= max_radius and not self.flag_to_delay:
                 self.flag_to_delay = True
@@ -314,7 +302,8 @@ class PlayLevel(GameStrategy):
 
             if self.win_offsets[0][1] >= max_radius and pygame.time.get_ticks() - self.delay >= 1000:
                 for offset1 in boarder_offsets:
-                    pygame.draw.circle(self.screen, (0, 50, 30), offset1, self.circle_radius)
+                    pygame.draw.circle(
+                        self.screen, self.current_palette.pixels[3][6], offset1, self.circle_radius)
                 self.circle_radius += 8
 
             if self.circle_radius >= 650:
