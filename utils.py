b2acddb9eb98e255e49a6ac72e1984da523a7f6d
quswadress <quswadress@gmail.com>
2022-04-04 13:00:16 +0300

    inf movement

--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
+from typing import Final, Literal, Dict
+
+import pygame
+
+
 def my_deepcopy(arr):
     """Полное копирование трёхмерного массива без использования указателей
 
@@ -13,3 +18,21 @@ def my_deepcopy(arr):
         else:
             new_arr.append(val)
     return new_arr
+
+
+def get_pressed_direction() -> \
+        Literal[-1, 0, 1, 2, 3]:
+    """Метод обработки клавиш поворота чего-либо"""
+    side_and_key: Dict[int, Literal[0, 1, 2, 3]] = {
+        pygame.K_d: 0,
+        pygame.K_w: 1,
+        pygame.K_a: 2,
+        pygame.K_s: 3
+    }
+    bad: Final[Literal[-1]] = -1
+    turning_side: Literal[-1, 0, 1, 2, 3] = bad
+    for key in side_and_key.keys():
+        if pygame.key.get_pressed()[key]:
+            turning_side = side_and_key[key]
+            break
+    return turning_side
