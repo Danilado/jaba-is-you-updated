2f584c3e7aad5fa1bcb35a3f5523891f66f3b318
Горбунов Константин <quswadress@gmail.com>
2023-11-15 22:12:01 +0000

    Little optimization

--- utils.py
+++ utils.py
@@ -1,25 +1,7 @@
-from copy import copy
+import os
 from typing import Final, Literal, Dict
 
-import os
 import pygame
-
-
-def my_deepcopy(arr):
-    """Полное копирование трёхмерного массива без использования указателей
-
-    :param arr: Исходный массив
-    :type arr: list
-    :return: Копия в других ячейках памяти
-    :rtype: list
-    """
-    new_arr = []
-    for val in arr:
-        if isinstance(val, list):
-            new_arr.append(my_deepcopy(val))
-        else:
-            new_arr.append(copy(val))
-    return new_arr
 
 
 def get_pressed_direction(arrows: bool = False) -> \
